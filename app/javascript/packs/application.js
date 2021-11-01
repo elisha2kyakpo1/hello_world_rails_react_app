8ff6f839cc88f4ecfec14f66eaadfffec640fec4
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-01 12:02:19 +0200

    Fix webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,14 @@
 import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
+import 'semantic-ui-css/semantic.min.css';
 import 'channels';
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
 // Support component names relative to this directory:
-const componentRequireContext = require.context("components", true);
-const ReactRailsUJS = require("react_ujs");
+const componentRequireContext = require.context('components', true);
+const ReactRailsUJS = require('react_ujs');
+
 ReactRailsUJS.useContext(componentRequireContext);
