fccf3af4c185bdad56e53fb01d7a0f9e30fbc62e
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-11-01 12:22:15 +0200

    Add seed for default data

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { List, Button } from 'semantic-ui-react';
 import { getMessages } from '../redux/Messages';
-import './App.css';
 
 const Greeting = () => {
   const messages = useSelector((state) => state.messageReducer);
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+messages = [
+  "Hello, good morging my good friend",
+  "Hello, I will be a bit late today",
+  "Hello, I will buy food for everyone",
+  "Bashh! I'm tired like a fool",
+  "Hey, the code is full of bugs"
+]
+
+messages.each do |greeting|
+  Message.create( greeting: greeting)
+end
